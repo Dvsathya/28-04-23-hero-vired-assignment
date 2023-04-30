98811640f68ced78d3a8d024fb6846d77a8c835a
sathya <satyavardhanreddy12@gmail.com>
2023-04-30 10:45:19 +0530

    1

--- invoice amount.py
+++ invoice amount.py
@@ -9,3 +9,4 @@ book3 = int(input("Enter number of data science in  python  books yoy want : "))
 total_price = book1*python_p + book2*python_l_price + book3*ds_price
 total_invoice_amount =total_price + total_price * gst + delivery_charges
 print("Total invoice amount = ",total_invoice_amount)
+#total invoice amount
