6b4cc8a3ce38506921080df97f4d399d704af2ea
sathya <satyavardhanreddy12@gmail.com>
2023-04-30 10:43:30 +0530

    first commit

---
+++ unique letter.py
@@ -0,0 +1,12 @@
+s = input("Enter string : ")
+str1 = s.lower()
+list1 = []
+for i in str1:
+    if i not in list1:
+        list1.append(i)
+print(list1)
+for x in range(len(list1)):
+    if x != len(list1) - 1:
+        print(list1[x],end = ",")
+    else:
+        print(list1[x])
